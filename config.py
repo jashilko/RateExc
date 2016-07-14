07a5e631beb6ea2f3a3966992ebf02b031175cea
jashilko <jashilko@gmail.com>
2016-07-14 15:18:19 +0000

    Снова удаляем индексацию

--- config.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-token = '233444946:AAFMuF8yt0djxpvDAgICS2olByy8xHbtLQ8'  # Токен бота
-shelve_status = 'status.db' # Файл с хранилищем статуса.
-shelve_orderid = 'orderid.db' # Файл с хранилищем статуса.
